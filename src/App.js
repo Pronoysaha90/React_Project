83d3722b68c85c9cacd2d10533f9c0966cf25f6a
pronoy <pronoysaha623@gmail.com>
2025-11-25 22:12:12 +0600

    compopents

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
-
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Navber from "./components/navber";
+import Info from './components/info';
 
 function App() {
   return (
-    <div className="App">
-          <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    </div>
+    <>
+     <Navber/ >
+     <Info name = "Pronoy" age = "24" />
+     <Info name = "Anik" age = "27" />
+     <Info name = "Abir" age = "28" />
+     
+    </>
   );
 }
 
---
+++ src/components/info.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function info(props) {
+  return (
+    <div>
+      My name is {props.name} and age {props.age}
+    </div>
+  )
+}
+
+export default info
